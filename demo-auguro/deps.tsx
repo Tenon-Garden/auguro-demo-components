11dff8ee9b82072c271581b655e8527ffa70a482
Tommy Vegetables <leafyrabbet@gmail.com>
2020-08-22 19:48:14 -0400

    ENH: Simple React.js Demo
    - Enhanced original files to show a simple React.js demo with an
    interactive button.
    - Tested locally on macOS and confirmed functionality in Chrome.
    - Has 2 warnings and is running on development version of React.js, but
    otherwise works as expected.

--- demo-auguro/deps.tsx
+++ demo-auguro/deps.tsx
@@ -1,2 +1,3 @@
+export { default as React } from "https://dev.jspm.io/react@16.13.1";
+export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1/server";
 export { serve } from "https://deno.land/std/http/server.ts";
-
--- demo-auguro/server.tsx
+++ demo-auguro/server.tsx
@@ -1,14 +1,66 @@
-import { serve } from "./deps.tsx";
+import { 
+   serve,
+   React,
+   ReactDOMServer,
+} from "./deps.tsx";
+
+import App from "./app.tsx";
+
+
+const DEBUG_MODE = false;
+
+
+const server_config = {
+   host: "localhost",
+   port: 8000,
+};
 
 const s = serve(
-   {
-      port: 8000,
-   },
+   server_config,
 );
 
-console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`);
+console.log(
+   `🦕 Deno server running...
+🦕 http://${server_config.host}:${server_config.port}/
+🦕 Press CTRL+C to quit...
+`
+);
+
+const js_filename = "demo.js";
+const js = `import React from "https://dev.jspm.io/react@16.13.1";
+import ReactDOM from "https://dev.jspm.io/react-dom@16.13.1";
+const App = ${App};
+ReactDOM.hydrate(React.createElement(App), document.body);`;
+const js_content = new TextEncoder().encode(js);
+
+const html_content = `<html>
+<head>
+  <script type="module" src="${js_filename}"></script>
+  <style>* { font-family: Helvetica; }</style>
+</head>
+<body>
+   ${(ReactDOMServer as any).renderToString(<App />)}
+</body>
+</html>`;
+
+const body_content = new TextEncoder().encode(html_content);
 
 for await (const req of s) {
-  req.respond({ body: 'Hello from your first Deno server' });
-}
-
+   if (DEBUG_MODE) {
+      console.log(req.url);
+   };
+   if (req.url === `/${js_filename}`) {
+      const resp_obj = {
+         body: js_content,
+         headers: new Headers(
+            { "content-type": "text/javascript", },
+         ),
+      };
+      req.respond(resp_obj);
+   } else {
+      const resp_obj = {
+         body: body_content,
+      };
+      req.respond(resp_obj);
+   }
+};
