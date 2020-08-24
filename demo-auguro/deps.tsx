1ba39762cb9f299fcd2a4db971d427e17993b599
Tommy Vegetables <leafyrabbet@gmail.com>
2020-08-24 19:28:25 -0400

    FIX: handlebars broke Dependencies
    - `handlebars` isn't working with `std@0.66.0` in deno, so this commit
    replaces it with another third-party import: `view_engine`.

--- demo-auguro/deps.tsx
+++ demo-auguro/deps.tsx
@@ -1,10 +1,11 @@
 // Standard Library
-export { serve } from "https://deno.land/std/http/server.ts";
-export { serveFile } from "https://deno.land/std/http/file_server.ts";
+export { serve } from "https://deno.land/std@0.66.0/http/server.ts";
+export { serveFile } from "https://deno.land/std@0.66.0/http/file_server.ts";
 
 // React.js
 export { default as React } from "https://dev.jspm.io/react@16.13.1";
 export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1/server";
 
 // Third-Party
-import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
+// import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
+export { engineFactory } from "https://deno.land/x/view_engine@v1.3.0/mod.ts";
