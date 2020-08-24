38eee8b9bd7a6f0aee1d55a6b15cb72438e6b407
Tommy Vegetables <leafyrabbet@gmail.com>
2020-08-24 18:41:07 -0400

    UPD: Dependencies Updated
    - Adding `handlebars` (`mustache` templates) to dependencies.
    - Reorganizing dependencies.

--- demo-auguro/deps.tsx
+++ demo-auguro/deps.tsx
@@ -1,4 +1,10 @@
-export { default as React } from "https://dev.jspm.io/react@16.13.1";
-export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1/server";
+// Standard Library
 export { serve } from "https://deno.land/std/http/server.ts";
 export { serveFile } from "https://deno.land/std/http/file_server.ts";
+
+// React.js
+export { default as React } from "https://dev.jspm.io/react@16.13.1";
+export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1/server";
+
+// Third-Party
+import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
